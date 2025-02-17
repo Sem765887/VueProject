670e1035a4e4862cc35287f57002fa53cf8a3811
Sem765887 <semyon.gardt765887@mail.ru>
2025-02-17 10:46:01 +0700

    Практическая №3

--- vue/js/main.js
+++ vue/js/main.js
@@ -7,6 +7,8 @@ let app = new Vue ({
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inStock: true,
-        inventory: 100
+        inventory: 8,
+        onSale: false,
+        cost: 5
     }
 })
