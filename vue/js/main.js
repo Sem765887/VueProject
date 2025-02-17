5d441103a7ccc4a69ed887a3bd58aff05de16a7c
Sem765887 <semyon.gardt765887@mail.ru>
2025-02-17 11:09:24 +0700

    Практическая №4

--- vue/js/main.js
+++ vue/js/main.js
@@ -9,6 +9,18 @@ let app = new Vue ({
         inStock: true,
         inventory: 8,
         onSale: false,
-        cost: 5
+        cost: 5,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
     }
 })
